445c2855b002d0a5f2abe42c14bf64004901b95a
garvit singh bhatnagar <23f1001875@ds.study.iitm.ac.in>
2025-11-23 16:36:41 +0000

    added db models structs

---
+++ backend/models.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"time"
+	"gorm.io/gorm"
+)
+
+// users table structure
+type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	Token     string    `json:"token"` // storing login token here
+	CartID    int       `json:"cart_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// items that we sell
+type Item struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name"`
+	Status    string    `json:"status"` // like active or inactive
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// carts table
+type Cart struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	UserID    int       `json:"user_id"`
+	Name      string    `json:"name"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+	Items     []Item    `gorm:"many2many:cart_items;"` // relation for cart items
+}
+
+// final orders
+type Order struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	CartID    int       `json:"cart_id"`
+	UserID    int       `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
